0e18c87da481f3d8d275346b72ec96df138cd1e7
ProstoNiki <56969932+ProstoNiki@users.noreply.github.com>
2019-11-25 21:56:02 +0300

    Create Task8.cpp

---
+++ 2019/11.8/Task8.cpp
@@ -0,0 +1,23 @@
+//Напишите программу находящую количество несократимых правильных дробей со знаменателем n.
+#include <iostream>
+using namespace std;
+
+int proper_fraction(int n) {
+	int result = n;
+	for (int i = 2; i * i <= n; ++i)
+		if (n % i == 0) {
+			while (n % i == 0)
+				n /= i;
+			result -= result / i;
+		}
+	if (n > 1)
+		result -= result / n;
+	return result;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << proper_fraction(n) * 2;
+	return 0;
+}
